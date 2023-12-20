9eef1e72cf28376947102f42d6a92c182b31a997
KasunDimantha <kasundimantha97@gmail.com>
2023-12-20 16:08:46 +0530

    complete algorithm

--- Insertion_Sort.java
+++ Insertion_Sort.java
@@ -3,8 +3,34 @@ package Sorting_Algorithms;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Insertion_Sort {
+
+
+    public static int[] insertionSort(int[] inputArray){
+
+        int[] arr = inputArray.clone();
+        int n = arr.length;
+
+        for (int i=1; i<n; i++){
+            int key = arr[i];
+            int j = i-1;
+
+            while(j>=0 && arr[j] > key){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+        printReasult(inputArray, arr);
+        return arr;
+    }
+
+    public static void printReasult(int[] unsortedArray, int[] sortedArray){
+        System.out.println("Unsorted Array        : " + Arrays.toString(unsortedArray));
+        System.out.println("Sorted Array          : " + Arrays.toString(sortedArray));
+    }
 
 
     public static void main(String[] args) throws IOException{
@@ -30,6 +56,8 @@ public class Insertion_Sort {
         for (int i=0; i<n; i++){
             inputArray[i] = Integer.parseInt(numbers[i]);
         }
+
+        insertionSort(inputArray);
     }
     
 }
